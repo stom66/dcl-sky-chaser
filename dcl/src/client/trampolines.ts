01ab3e6a3f7675e59668c03334bc89899cd6cd36
stom <tomsteventon@gmail.com>
2026-06-22 03:50:48 +0100

    fix: correct forward direction, support box collider, add sfx

--- dcl/src/client/trampolines.ts
+++ dcl/src/client/trampolines.ts
@@ -1,13 +1,14 @@
 import { ColliderLayer, engine, Entity, MeshCollider, MeshRenderer, Physics, Transform, TriggerArea, triggerAreaEventsSystem } from "@dcl/sdk/ecs"
 import { Quaternion, Vector3 } from "@dcl/sdk/math"
+import { sfx, SoundManager } from "./soundManager"
 
 export namespace Trampolines {
 
 	const TRIGGER_RADIUS  = 5.5
 	const SHOW_TRIGGER    = false // false for prod
 
-	const IMPULSE_FORCE   = 80
-	const IMPULSE_TILT_UP = -20
+	const IMPULSE_FORCE   = 120
+	const IMPULSE_TILT_UP = -55
 
 	const VERTICAL_OFFSET = 1
 
@@ -16,26 +17,33 @@ export namespace Trampolines {
 		direction: Vector3
 
 		constructor(
-			pos: Vector3, 
-			rot: Quaternion
+			pos  : Vector3, 
+			rot  : Quaternion,
+			scale: Vector3 = Vector3.create(TRIGGER_RADIUS, TRIGGER_RADIUS, TRIGGER_RADIUS),
+			useCubeTrigger: boolean = false
 		) {
 			this.entity = engine.addEntity()
 			Transform.create(this.entity, { 
 				position: Vector3.create(pos.x, pos.y + VERTICAL_OFFSET, pos.z), 
 				rotation: rot, 
-				scale   : Vector3.create(TRIGGER_RADIUS, TRIGGER_RADIUS, TRIGGER_RADIUS) 
+				scale   : scale
 			})
 
-			const forward     = Vector3.rotate(Vector3.Forward(), rot)
-			const trueForward = Vector3.rotate(forward, Quaternion.fromEulerDegrees(0, -45, 0))
-			const tiltUp      = Vector3.rotate(trueForward, Quaternion.fromEulerDegrees(0, 0, IMPULSE_TILT_UP))
+			const tiltUp      = Vector3.rotate(Vector3.Backward(), Quaternion.fromEulerDegrees(-IMPULSE_TILT_UP, 0, 0))
+			const forward     = Vector3.rotate(tiltUp, rot)
 
-			this.direction    = trueForward
+			this.direction    = forward
 
 			// Add a spherical collider trigger
 			//MeshCollider.setSphere(this.entity)
 
-			TriggerArea.setSphere(this.entity)
+			// if we have a scale, then we should use a cube
+			if (useCubeTrigger) {
+				TriggerArea.setBox(this.entity)
+				//MeshRenderer.setBox(this.entity)
+			} else {
+				TriggerArea.setSphere(this.entity)
+			}
 
 			// Add a visual sphere for debugging
 			if (SHOW_TRIGGER) MeshRenderer.setSphere(this.entity)
@@ -52,6 +60,7 @@ export namespace Trampolines {
 			console.log("Trampoline: Player entered")
 			// Apply an impulse to the player
 			Physics.applyImpulseToPlayer(this.direction, IMPULSE_FORCE)
+			SoundManager.playSound(sfx.boing)
 		}
 	
 		onTriggerExit() {
@@ -61,27 +70,30 @@ export namespace Trampolines {
 
 
 	const transforms = [
-		// trampoline
-		{
-			position: Vector3.create(264.476, 63.049, 263.725),
-			rotation: Quaternion.fromEulerDegrees(0, 57.341, 0),
-		},
 		// trampoline.001
 		{
-			position: Vector3.create(263.698, 63.049, 246.848),
-			rotation: Quaternion.fromEulerDegrees(0, 296.941, 0),
+			position: Vector3.create(262.903, 63.049, 246.848),
+			rotation: Quaternion.fromEulerDegrees(0, 45, 0),
 		},
 		// trampoline.002
 		{
-			position: Vector3.create(244.542, 63.049, 257.475),
-			rotation: Quaternion.fromEulerDegrees(0, 238.03, 0),
-		},
+			position: Vector3.create(245.332, 63.049, 256.61),
+			rotation: Quaternion.fromEulerDegrees(0, 90, 0),
+		}
 	]
 	
 
     export function init() {
         for (const transform of transforms) {
-            new Trampoline(transform.position, transform.rotation)
+            new Trampoline(transform.position, transform.rotation, )
         }
+
+		// Also make the awning a trampoline
+		const awningTranform = {
+			position: Vector3.create(258.163, 56.5, 278.01),
+			rotation: Quaternion.fromEulerDegrees(-24, 180, 0),
+			scale   : Vector3.create(8.6, 0.75, 6.9),
+		}
+		new Trampoline(awningTranform.position, awningTranform.rotation, awningTranform.scale, true)
     }
 }
