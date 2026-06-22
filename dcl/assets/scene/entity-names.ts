e8b2221c0e15196bec37f8784403cf29c37175ca
stom <tomsteventon@gmail.com>
2026-06-22 03:00:08 +0100

    feat: birds, latest tower, leaderboard lever

--- dcl/assets/scene/entity-names.ts
+++ dcl/assets/scene/entity-names.ts
@@ -5,6 +5,9 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  bird01_gltf = "bird01.gltf",
+  bird02_gltf = "bird02.gltf",
+  bird03_gltf = "bird03.gltf",
   clouds_gltf = "clouds.gltf",
   errorSign_gltf = "errorSign.gltf",
   tower_gltf = "tower.gltf",
