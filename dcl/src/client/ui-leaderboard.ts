f2cf4c63ad6b1904b4b549a5585c1d326cf1d992
stom <tomsteventon@gmail.com>
2026-08-10 04:13:47 +0100

    feat: add avatarIcon and background to Leaderboard

--- dcl/src/client/ui-leaderboard.ts
+++ dcl/src/client/ui-leaderboard.ts
@@ -1,4 +1,4 @@
-import { Animator, engine, Entity, GltfContainer, InputAction, MeshCollider, MeshRenderer, pointerEventsSystem, PointerEventType, TextAlignMode, TextShape, Transform } from "@dcl/sdk/ecs";
+import { Animator, engine, Entity, GltfContainer, InputAction, Material, MaterialTransparencyMode, MeshCollider, MeshRenderer, pointerEventsSystem, PointerEventType, TextAlignMode, TextShape, Transform } from "@dcl/sdk/ecs";
 import { Color4, Quaternion, Vector3 } from "@dcl/sdk/math";
 import { UiEntity } from "@dcl/sdk/react-ecs";
 import { LeaderboardEntry } from "src/shared/classes/leaderboard";
@@ -12,6 +12,8 @@ export namespace UILeaderboard {
 
 	let currentLeaderboard: string = "Weekly"
 
+
+	// MARK: Init
 	export function init() {
 
 		updateLeaderboard(currentLeaderboard, ComponentStore.getLeaderboardWeekly())
@@ -66,6 +68,8 @@ export namespace UILeaderboard {
 		})
 	}
 
+
+	// MARK: Toggle Leaderboard
 	function toggleLeaderboard() {
 		if (currentLeaderboard === "Weekly") {
 			currentLeaderboard = "AllTime"
@@ -82,6 +86,8 @@ export namespace UILeaderboard {
 		}
 	}
 
+
+	// MARK: Update Leaderboard
 	function updateLeaderboard(title: string, entries: Omit<LeaderboardEntry, 'lastUpdated'>[]) {
 		removeAllEntities()
 
@@ -103,20 +109,28 @@ export namespace UILeaderboard {
 			return
 		}
 
+		// MARK: Row
 		for (const [index, entry] of entries.entries()) {
 			const rowPosition = Vector3.add(origin, Vector3.create(0, rowOffset, 0))
 			// Row offset gets smaller as index increases: first row gets full spacing, last row half.
 			const factor = 1 - (index / (entries.length - 1 || 1)) * 0.333
 			rowOffset -= rowSpacing * factor
 
+			// Background
+			addBackground(rowPosition, index % 2 === 0)
 
 			const fontSize = 3 - (index * 0.15)
 			// Rank
-			let colPosition = Vector3.add(rowPosition, Vector3.create(0, 0, -2.5))
+			let colPosition = Vector3.add(rowPosition, Vector3.create(0, 0, -2.25))
 			addTextShape(entry.rank.toString() ?? "~", colPosition, rotation, fontSize)
 
+
+			// Avatar
+			colPosition = Vector3.add(rowPosition, Vector3.create(0, 0, -1.8))
+			addAvatar(entry.userId ?? "", colPosition, rotation)
+
 			// Display Name
-			colPosition = Vector3.add(rowPosition, Vector3.create(0, 0, -2))
+			colPosition = Vector3.add(rowPosition, Vector3.create(0, 0, -1.5))
 			addTextShape(entry.displayName ?? "Unknown", colPosition, rotation, fontSize)
 
 			// Score
@@ -125,6 +139,22 @@ export namespace UILeaderboard {
 		}
 	}
 
+	function addBackground(position: Vector3, flip: boolean) {
+		const uiEntity = engine.addEntity()
+		entities.push(uiEntity)
+		Transform.create(uiEntity, {
+			position: Vector3.subtract(position, Vector3.create(0, 0.2, -0.05)),
+			rotation: Quaternion.fromEulerDegrees(0, 90, flip ? 180 : 0),
+			scale   : Vector3.create(5, 0.4, 1),
+		})
+		MeshRenderer.setPlane(uiEntity)
+		Material.setPbrMaterial(uiEntity, {
+			texture         : Material.Texture.Common({ src: "assets/tex/bg-scoreboard.png" }),
+			alphaTest       : 0.2,
+			transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND,
+			castShadows     : false,
+		})
+	}
 
 	function addTextShape(
 		text    : string, 
@@ -148,6 +178,23 @@ export namespace UILeaderboard {
 			shadowBlur   : 0.5,
 			shadowOffsetX: 1,
 			shadowOffsetY: -1,
+		})
+	}
+
+	function addAvatar(userId: string, position: Vector3, rotation: Quaternion) {
+		const uiEntity = engine.addEntity()
+		entities.push(uiEntity)
+		Transform.create(uiEntity, {
+			position: Vector3.subtract(position, Vector3.create(0, 0.2, 0)),
+			rotation: rotation,
+			scale   : Vector3.create(0.4, 0.4, 0.4),
+		})
+		MeshRenderer.setPlane(uiEntity)
+		Material.setPbrMaterial(uiEntity, {
+			texture         : Material.Texture.Avatar({ userId }),
+			transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND,
+			castShadows     : false,
+
 		})
 	}
 
