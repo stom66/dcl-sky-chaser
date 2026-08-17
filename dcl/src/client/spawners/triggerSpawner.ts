06e6175cdb318d22cebd7ca8598f6c941a6f9687
stom <tomsteventon@gmail.com>
2026-08-17 03:22:34 +0100

    fix: increase death trigger height

--- dcl/src/client/spawners/triggerSpawner.ts
+++ dcl/src/client/spawners/triggerSpawner.ts
@@ -6,7 +6,7 @@ export namespace TriggerSpawner {
 	var deathTriggers: DeathTrigger[] = []
 
     export function spawnTriggers() {
-        const floorDeathTrigger = new DeathTrigger(Vector3.create(256, 8, 256), Vector3.create(512, 16, 512))
+        const floorDeathTrigger = new DeathTrigger(Vector3.create(256, 11, 256), Vector3.create(512, 22, 512))
 		deathTriggers.push(floorDeathTrigger)
     }
 }
