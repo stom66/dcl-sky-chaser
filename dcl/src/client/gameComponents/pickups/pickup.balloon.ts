eb75081deb783dc198024d1ecc1f1760d944dca1
stom <tomsteventon@gmail.com>
2026-08-27 15:33:16 +0100

    fix: balloon pickup noises are now spatial, not global

--- dcl/src/client/gameComponents/pickups/pickup.balloon.ts
+++ dcl/src/client/gameComponents/pickups/pickup.balloon.ts
@@ -226,7 +226,7 @@ export class PickupBalloon extends Pickup {
 		}
 
 		if (!silent) {
-			SoundManager.playSound(sfx.balloonPickup)
+			SoundManager.playSound(sfx.balloonPickup, this.rootEntity)
 		}
 	}
 
